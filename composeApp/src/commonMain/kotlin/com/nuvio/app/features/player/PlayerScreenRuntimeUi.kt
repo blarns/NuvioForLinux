61218bc36ca4766bc49066b05a68b20abacf20db
tapframe <85391825+tapframe@users.noreply.github.com>
2026-06-09 02:01:23 +0530

    Fix credentialed HLS playback refresh

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/player/PlayerScreenRuntimeUi.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/player/PlayerScreenRuntimeUi.kt
@@ -146,6 +146,9 @@ internal fun PlayerScreenRuntime.RenderPlayerRuntimeUi() {
                     }
                 },
                 onError = { message ->
+                    if (message != null && tryRefreshCredentialedSourceAfterError(message)) {
+                        return@PlatformPlayerSurface
+                    }
                     errorMessage = message
                     if (message != null) {
                         controlsVisible = !playerControlsLocked
